c62aa839740e98f6df11cede0ecdf14649ec998f
Leomotors (Ubuntu) <59821765+Leomotors@users.noreply.github.com>
2021-10-05 14:34:27 +0700

    Release 1.1.0

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+# Change Log
+
+All notable changes to the "auto-publish-release" extension will be documented in this file.
+
+## [1.1.0] - 2021-10-05
+
+- Now detects CHANGELOG.md (this exact form) and add to release body, see the code to know how it is detected
+
+## [1.0.0] - 2021-09-27
+
+- Initial release
--- index.js
+++ index.js
@@ -3,34 +3,65 @@ const github = require("@actions/github");
 const fs = require("fs").promises;
 
 async function getVersion() {
-    const buffer = await fs.readFile("package.json");
-    const package_data = JSON.parse(buffer.toString());
-    const version = package_data.version;
+  const buffer = await fs.readFile("package.json");
+  const package_data = JSON.parse(buffer.toString());
+  const version = package_data.version;
 
-    return version;
+  // * X.Y.Z => 5..
+  if (version.length < 5) throw new Error("Invalid Version");
+
+  return version;
+}
+
+async function getChangelog(version) {
+  try {
+    const buffer = await fs.readFile("CHANGELOG.md");
+    const lines = buffer.toString().split("\n");
+
+    let body = "";
+
+    let startlevel = 0;
+    for (const line of lines) {
+      if (line.startsWith("#") && line.includes(version)) {
+        startlevel = line.split("").filter((c) => c == "#").length;
+      }
+
+      if (startlevel) {
+        if (line.startsWith("#".repeat(startlevel)) && !line.includes(version))
+          return body;
+        body += line + "\n";
+      }
+    }
+    return body;
+  } catch (err) {
+    return null;
+  }
 }
 
 async function run() {
-    try {
-        const version = await getVersion();
+  try {
+    const version = await getVersion();
 
-        const ghToken = core.getInput("GITHUB_TOKEN");
-        const octokit = github.getOctokit(ghToken);
+    const ghToken = core.getInput("GITHUB_TOKEN");
+    const octokit = github.getOctokit(ghToken);
 
-        const { owner, repo } = github.context.repo;
+    const { owner, repo } = github.context.repo;
 
-        const ReleaseName = `Release ${version}`;
+    const ReleaseName = `Release ${version}`;
 
-        await octokit.request("POST /repos/{owner}/{repo}/releases", {
-            owner,
-            repo,
-            tag_name: version,
-            name: ReleaseName
-        });
-    }
-    catch (error) {
-        core.setFailed(error.message);
-    }
+    const body =
+      (await getChangelog(version)) ?? core.getInput("CHANGELOG_BODY");
+
+    await octokit.request("POST /repos/{owner}/{repo}/releases", {
+      owner,
+      repo,
+      tag_name: version,
+      name: ReleaseName,
+      body,
+    });
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
 
 run();
