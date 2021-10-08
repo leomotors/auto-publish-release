40df0c5055d624bfafa2379b0493e558a9b106d1
Leomotors (Ubuntu) <59821765+Leomotors@users.noreply.github.com>
2021-10-08 22:10:45 +0700

    1.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 
 All notable changes to the "auto-publish-release" extension will be documented in this file.
 
+## [1.2.0] - 2021-10-08
+
+- Now detects pre-release version and release as it should be
+
 ## [1.1.0] - 2021-10-05
 
 - Now detects CHANGELOG.md (this exact form) and add to release body, see the code to know how it is detected
--- index.js
+++ index.js
@@ -3,65 +3,76 @@ const github = require("@actions/github");
 const fs = require("fs").promises;
 
 async function getVersion() {
-  const buffer = await fs.readFile("package.json");
-  const package_data = JSON.parse(buffer.toString());
-  const version = package_data.version;
+    const buffer = await fs.readFile("package.json");
+    const package_data = JSON.parse(buffer.toString());
+    const version = package_data.version;
 
-  // * X.Y.Z => 5..
-  if (version.length < 5) throw new Error("Invalid Version");
+    // * X.Y.Z => 5..
+    if (version.length < 5) throw new Error("Invalid Version");
 
-  return version;
+    return version;
+}
+
+function versionIsPrerelease(version) {
+    for (const kw of ["alpha", "beta", "dev", "pre", "rc", "insider"])
+        if (version.includes(kw)) return true;
+    return false;
 }
 
 async function getChangelog(version) {
-  try {
-    const buffer = await fs.readFile("CHANGELOG.md");
-    const lines = buffer.toString().split("\n");
+    try {
+        const buffer = await fs.readFile("CHANGELOG.md");
+        const lines = buffer.toString().split("\n");
 
-    let body = "";
+        let body = "";
 
-    let startlevel = 0;
-    for (const line of lines) {
-      if (line.startsWith("#") && line.includes(version)) {
-        startlevel = line.split("").filter((c) => c == "#").length;
-      }
+        let startlevel = 0;
+        for (const line of lines) {
+            if (line.startsWith("#") && line.includes(version)) {
+                startlevel = line.split("").filter((c) => c == "#").length;
+            }
 
-      if (startlevel) {
-        if (line.startsWith("#".repeat(startlevel)) && !line.includes(version))
-          return body;
-        body += line + "\n";
-      }
+            if (startlevel) {
+                if (
+                    line.startsWith("#".repeat(startlevel)) &&
+                    !line.includes(version)
+                )
+                    return body;
+                body += line + "\n";
+            }
+        }
+        return body;
+    } catch (err) {
+        return null;
     }
-    return body;
-  } catch (err) {
-    return null;
-  }
 }
 
 async function run() {
-  try {
-    const version = await getVersion();
+    try {
+        const version = await getVersion();
+        const prerelease = versionIsPrerelease(version);
 
-    const ghToken = core.getInput("GITHUB_TOKEN");
-    const octokit = github.getOctokit(ghToken);
+        const ghToken = core.getInput("GITHUB_TOKEN");
+        const octokit = github.getOctokit(ghToken);
 
-    const { owner, repo } = github.context.repo;
+        const { owner, repo } = github.context.repo;
 
-    const ReleaseName = `Release ${version}`;
+        const ReleaseName = `Release ${version}`;
 
-    const body =
-      (await getChangelog(version)) || core.getInput("CHANGELOG_BODY");
+        const body =
+            (await getChangelog(version)) || core.getInput("CHANGELOG_BODY");
 
-    await octokit.request("POST /repos/{owner}/{repo}/releases", {
-      owner,
-      repo,
-      tag_name: version,
-      name: ReleaseName,
-      body,
-    });
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+        await octokit.request("POST /repos/{owner}/{repo}/releases", {
+            owner,
+            repo,
+            tag_name: version,
+            name: ReleaseName,
+            body,
+            prerelease,
+        });
+    } catch (error) {
+        core.setFailed(error.message);
+    }
 }
 
 run();
